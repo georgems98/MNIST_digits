9a6fac2f5c1520ed4fea5c6e7d54bd05a34bdeb9
George Molina-Stubbs <gmolinastubbs@gmail.com>
2020-06-19 19:54:36 +0100

    Completed version of the _explore program

--- mnist_explore.py
+++ mnist_explore.py
@@ -9,16 +9,18 @@ of digits, a sample of which can be displayed with matplotlib.
 GAUSSIAN NAIVE BAYES
 --------------------
 A quick and easy Gaussian naive Bayes is fitted to obtain an 83% accuracy 
-rating. This is pretty poor, but we would expect this 
+rating. This is pretty poor, but we would expect this as pixel values won't 
+be independent.
 
 
 GRADIENT BOOSTED DECISION TREES
 -------------------------------
 A more sophisticated model of gradient boosted decision trees is fitted to 
-obtain a 95% accuracy rating on the test set.
+obtain a 96% accuracy rating on the test set. A large improvement over NB, but
+still 1/25 digits misclassified.
 
 
-@author: George Molina-Stubbs
+@author: georgems
 """
 
 
@@ -54,7 +56,7 @@ fig, axes = plt.subplots(10, 10, figsize=(8,8),
 # Iterate over each ax in the axes array
 for i, ax in enumerate(axes.flat):
     # Display the digit image in the given ax
-    ax.imshow(Xtest[i], cmap='binary', interpolation='nearest')
+    ax.imshow(Xtest[i].reshape(8,8), cmap='binary', interpolation='nearest')
     
     # Add the correct label in the corner
     ax.text(0.05, 0.05, str(ytest[i]), transform = ax.transAxes, 
@@ -62,21 +64,20 @@ for i, ax in enumerate(axes.flat):
     
 
 #%% ==== DIMENSION REDUCE ====
+# Project the 64 dim predictor space into 2D to get a feel for the data
 from sklearn.manifold import Isomap
 
-# Initialise
 iso = Isomap(n_components = 2)
 
 # Fit to data and create the transformed data
 iso.fit(digits.data)
 data_projected = iso.transform(digits.data)
 
-# Plot the 2D projected data with points coloured to match
+# Plot the 2D projected data with points coloured to match classifications
 plt.scatter(data_projected[:,0], data_projected[:,1], c=digits.target,
             edgecolor='none', alpha = 0.6, 
             cmap=plt.cm.get_cmap('Paired', 10))
 
-# Add colour bar
 plt.colorbar(label='digit label', ticks = range(10))
 plt.clim(-0.5, 9.5)
 
@@ -86,15 +87,13 @@ plt.clim(-0.5, 9.5)
 # Select model
 from sklearn.naive_bayes import GaussianNB
 
-# Initialise
-mod_NB = GaussianNB()
 
-# Fit to data
+mod_NB = GaussianNB()
 mod_NB.fit(Xtrain, ytrain)
 
 # Predictions on test data
-y_mod_NB = mod_NB.predict(Xtest)
-accuracy_score(ytest, y_mod_NB) # 0.8333333333333334
+y_pred_NB = mod_NB.predict(Xtest)
+accuracy_score(ytest, y_pred_NB) # 0.8333333333333334
 
 
 # ---- PREDICTION PLOTS
@@ -129,15 +128,14 @@ def plot_sample(y_pred):
                 color='green' if (ytest[i] == y_pred[i]) else 'red')
 
 
-plot_conf_mat(y_mod_NB)
-plot_sample(y_mod_NB)
+plot_conf_mat(y_pred_NB)
+plot_sample(y_pred_NB)
     
-    
-'''
-Not a great classifier (83% accuray) but not bad for a first attempt with a 
-quick fit with naive Bayes
-'''
-
+"""
+Not a great classifier (83% accuray) but not bad for an easily fitted first 
+attempt. Considering we're working with image data we wouldn't expect the 
+pixel values to be independent anyway.
+"""
 
 
 #%% ==== GRADIENT BOOSTING CLASSIFIER ====
@@ -149,47 +147,105 @@ Xtest = Xtest.astype("float32")/255
 from sklearn.ensemble import GradientBoostingClassifier
 
 
-# ---- TUNE HYPERPARAMETERS
-n_range = np.arange(1,10)
-val_train, val_test = validation_curve(GradientBoostingClassifier(verbose=1),
-                                       Xtrain, ytrain, 
-                                       "n_estimators", n_estimators)
+#---- TUNE HYPERPARAMETERS
+#%% How many trees?
 
-plt.plot(n_range, np.median(val_train, 1), color='blue', label='training score')
-plt.plot(n_range, np.median(val_test, 1), color='red', label='validation score')
-plt.legend(loc='best')
+def val_plot(mod, param, param_range):
+    val_train, val_test = validation_curve(mod, Xtrain, ytrain, 
+                                           param, param_range,
+                                           cv=3)
+    
+    plt.figure()
+    plt.plot(param_range, np.median(val_train, 1), color='blue', label='training score')
+    plt.plot(param_range, np.median(val_test, 1), color='red', label='validation score')
+    plt.legend(loc='best')
+    plt.xlabel(param)
+    plt.ylabel("accuracy")
+    plt.title("Accuracy vs " + param)
+    
+    
 
 
+n_estimators = np.arange(40,200,20)
+mod = GradientBoostingClassifier(verbose=1)
+val_plot(mod, "n_estimators", n_estimators)
+
+"""
+Somewhere in the range of 80 - 120 seems good, with peak val score at 110
+"""
 
 
-
+#%% Randomised search across a range of parameters
 learning_rate = [0.005, 0.01, 0.1, 0.2]
-n_estimators = np.arange(20,200,40)
-max_depth = np.arange(1,10)
-min_samples_split = np.arange(2,10)
+n_estimators = np.arange(100,120,5)
+max_depth = np.arange(1,20)
+min_samples_split = np.arange(2,20)
 
-# Try grid search over the boosting parameters
 param_grid = dict(learning_rate = learning_rate,
-                  n_estimators = n_estimators)
+                  n_estimators = n_estimators,
+                  max_depth = max_depth,
+                  min_samples_split = min_samples_split)
 
 grid = RandomizedSearchCV(GradientBoostingClassifier(verbose=1), param_grid)
 grid.fit(Xtrain, ytrain)
 
 grid.cv_results_
 
+"""
+The top three combinations are
+  {'n_estimators': 110, 'min_samples_split': 8, 'max_depth': 5, 'learning_rate': 0.1},
+  {'n_estimators': 100, 'min_samples_split': 10, 'max_depth': 10, 'learning_rate': 0.2},
+  {'n_estimators': 110, 'min_samples_split': 7, 'max_depth': 12, 'learning_rate': 0.1}
+"""
+
+
+#%% Max depth and minimum samples to split on?
+mod = GradientBoostingClassifier(n_estimators=110, learning_rate = 0.1,
+                                 verbose=1)
+
+max_depth = np.arange(4,13,2)
+val_plot(mod, "max_depth", max_depth)
+
+min_samples_split = np.arange(2,11,2)
+val_plot(mod, "min_samples_split", min_samples_split)
+
+"""
+Looks like max_depth in range 3-7 and min_samples_split less than 5 is good.
+We take the lower end of these for simplicity.
+"""
+
+
+#%% Size of subsampling?
+param_grid = dict(subsample = np.arange(0.6,1,0.05))
+
+grid = GridSearchCV(GradientBoostingClassifier(verbose=1, 
+                                                     learning_rate=0.1, 
+                                                     n_estimators = 110,
+                                                     max_depth=3,
+                                                     min_samples_split=2), 
+                          param_grid, cv = 3)
+grid.fit(Xtrain, ytrain)
+
+grid.cv_results_ 
+
+"""
+{'subsample': 0.65}
+"""
+
+
+#%% ---- FIT MODEL
+mod_GB = GradientBoostingClassifier(learning_rate=0.1, n_estimators=110,
+                                    max_depth=3, min_samples_split=2,
+                                    subsample=0.65,
+                                    verbose=1)
+mod_GB.fit(Xtrain, ytrain)
+
+y_pred_GB = mod_GB.predict(Xtest)
+print(accuracy_score(ytest, y_pred_GB))     # 0.9644444444444444
 
 
 #%%
-# Fit to data
-mod_GB = GradientBoostingClassifier(verbose=1)
-mod_GB.fit(Xtrain, ytrain)
-
-# Predictions on test data
-y_mod_GB = mod_GB.predict(Xtest)
-accuracy_score(ytest, y_mod_GB) # 0.9555555555555556
-
-
-
+# Plot learning curve against dataset size
 N, train_lc, test_lc = learning_curve(mod_GB, 
                                       Xtrain, ytrain, 
                                       train_sizes=np.linspace(0.3,1,5),
@@ -199,55 +255,19 @@ plt.plot(N,np.mean(train_lc,1))
 plt.plot(N,np.mean(test_lc,1))
 plt.legend(["train", "test"])
 
+"""
+Looks like we haven't converged with this training data
+"""
 
 
-
-# ---- PREDICTION PLOTS
-plot_conf_mat(y_mod_GB)
-plot_sample(y_mod_GB)
-
+#%% ---- PREDICTION PLOTS
+plot_conf_mat(y_pred_GB)
+plot_sample(y_pred_GB)
 
 '''
 A significant improvement over the Gaussian naive Bayes!
-However 95% is still not great....
+However 96% is still not great...
+We need a more sophisticated model, e.g. a CNN that can use the inherent 
+spatial information in the image.
 '''
-
-
-
-#%% ---- Tune the model hyperparameters
-# Compute test/train scores for different max_depth parameters
-n_range = np.arange(1,10)
-val_train, val_test = validation_curve(mod_GB, Xtest, ytest, 'max_depth', n_range )
-
-plt.plot(n_range, np.median(val_train, 1), color='blue', label='training score')
-plt.plot(n_range, np.median(val_test, 1), color='red', label='validation score')
-plt.legend(loc='best')
-
-''' 
-The plot suggests max_depth = 2 or 3 gives best validation score
-'''
-
-# Try grid search over the boosting parameters
-param_grid = {'n_estimators': np.array([50,75,100,125]),
-           'learning_rate': np.array([0.05, 0.075, 0.1, 0.25, 0.5, 1])}
-
-grid = GridSearchCV(GradientBoostingClassifier(), param_grid)
-
-grid.fit(Xtrain, ytrain)
-grid.best_params_ # {'learning_rate': 0.1, 'n_estimators': 125}
-
-
-# Refit model with adjusted parameters
-mod_GB2 = grid.best_estimator_.fit(Xtrain, ytrain)
-
-y_mod_GB2 = mod_GB2.predict(Xtest)
-accuracy_score(ytest, y_mod_GB2) # 0.9555555555555556
-
-plot_conf_mat(y_mod_GB2)
-
-'''
-Not much improvement over the default parameters - should try a different 
-method
-'''
-
 
